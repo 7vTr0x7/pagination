12c40ba1a1197f848fe5c37696ddd72d65d90ea0
7vTr0x7 <7vtrox7@gmail.com>
2024-04-11 16:13:54 +0530

    fetched data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProductData = async () => {
+    const data = await fetch("https://dummyjson.com/products?limit=100");
+    const json = await data.json();
+    setProducts(json);
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  }, []);
+
   return <div>App</div>;
 };
 
